954e63853f2b175158e11a4522670ebfea70b9fe
Thuyen Code <thuyencode@proton.me>
2024-06-15 10:56:27 +0700

    Config Tailwind CSS and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
+import simpleHtmlPlugin from 'vite-plugin-simple-html'
+import { webfontDownload } from 'vite-plugin-webfont-dl'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/shopping-cart-the-odin-project/',
+  plugins: [react(), simpleHtmlPlugin({ minify: true }), webfontDownload()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')

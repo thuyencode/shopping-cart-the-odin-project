55193d58aaf0ec90bd03bb221f4e010b204231ee
Thuyen Code <thuyencode@proton.me>
2024-06-15 18:23:49 +0700

    Define new TypeScript types

---
+++ src/lib/types.ts
@@ -0,0 +1,51 @@
+export interface Product {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: Category
+  image: string
+  rating: {
+    rate: number
+    count: number
+  }
+}
+
+export type Category =
+  | 'electronics'
+  | 'jewelery'
+  | "men's clothing"
+  | "women's clothing"
+
+export interface Cart {
+  id: number
+  userId: number
+  date: string
+  products: Array<{
+    productId: number
+    quantity: number
+  }>
+  __v: 0
+}
+
+export interface User {
+  id: number
+  email: string
+  username: string
+  password: string
+  name: {
+    firstName: string
+    lastName: string
+  }
+  address: {
+    city: string
+    street: string
+    number: number
+    zipcode: string
+    geolocation: {
+      lat: string
+      long: string
+    }
+  }
+  phone: string
+}

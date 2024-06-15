b65eeed47f2b7be14d07426f2fbc8bad419e1a2e
Thuyen Code <thuyencode@proton.me>
2024-06-15 10:30:38 +0700

    Add more ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,29 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard-with-typescript",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs", "tailwind.config.js"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react-refresh", "react", "jsx-a11y"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "space-before-function-paren": "off",
+    "@typescript-eslint/space-before-function-paren": "off",
+    "multiline-ternary": "off",
+    "@typescript-eslint/indent": "off",
   },
-}
+};

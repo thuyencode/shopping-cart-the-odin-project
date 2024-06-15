8ac0ef0a273756d83caab60091525de28ef9fe9c
Thuyen Code <thuyencode@proton.me>
2024-06-15 11:42:31 +0700

    Add the base styles and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+import * as defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: []
 }

28a15678ed64a53e60acd8ad5793ea694fdb4d30
Thuyen Code <thuyencode@proton.me>
2024-06-16 09:26:21 +0700

    Add react-daisyui components library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import daisyui from 'daisyui'
 import * as defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js'
+  ],
   theme: {
     container: {
       center: true
@@ -13,5 +19,5 @@ export default {
       }
     }
   },
-  plugins: []
+  plugins: [daisyui]
 }

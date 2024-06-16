be20308bc33f79e77e449a70783b6011c2992428
Thuyen Code <thuyencode@proton.me>
2024-06-16 11:18:28 +0700

    Style the Navbar and home page with React Daisy UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,61 @@
+import { Icon } from '@iconify/react'
 import { type ReactElement } from 'react'
+import {
+  Badge,
+  Button,
+  Card,
+  Navbar as DaisyNavbar,
+  Dropdown,
+  Indicator
+} from 'react-daisyui'
 import { NavLink } from 'react-router-dom'
 
 function Navbar(): ReactElement {
+  function classNameWhenActive({ isActive }: { isActive: boolean }): string {
+    return isActive ? 'link link-primary no-underline' : 'link link-hover'
+  }
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'text-green-400' : '')}
-            to={'/'}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'text-green-400' : '')}
-            to={'/products'}
-          >
-            Products
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'text-green-400' : '')}
-            to={'/cart'}
-          >
-            Your Cart
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <DaisyNavbar className='flex-none'>
+      <div className='flex-1'>
+        <h3>The Fake Store</h3>
+      </div>
+      <div className='flex-none gap-5'>
+        <NavLink className={classNameWhenActive} to={'/'}>
+          <h4>Home</h4>
+        </NavLink>
+        <NavLink className={classNameWhenActive} to={'/products'}>
+          <h4>Products</h4>
+        </NavLink>
+        <Dropdown end>
+          <Button tag='label' tabIndex={0} color='ghost' shape='circle'>
+            <Indicator>
+              <Badge
+                size='sm'
+                color='primary'
+                className={Indicator.Item.className()}
+              >
+                8
+              </Badge>
+              <Icon className='text-xl' icon={'mdi:cart-variant'} />
+            </Indicator>
+          </Button>
+          <Dropdown.Menu className='card card-compact z-[1] mt-3 w-52 !p-0'>
+            <Card.Body>
+              <span className='text-lg font-bold'>8 Items</span>
+              <span className='text-info'>Subtotal: $999</span>
+              <Card.Actions>
+                <NavLink className='w-full' to={'/cart'}>
+                  <Button color='primary' fullWidth>
+                    View cart
+                  </Button>
+                </NavLink>
+              </Card.Actions>
+            </Card.Body>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </DaisyNavbar>
   )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,17 @@
 import { type ReactElement } from 'react'
+import { Button } from 'react-daisyui'
+import { Link } from 'react-router-dom'
 
 function HomePage(): ReactElement {
   return (
-    <>
+    <div className='flex flex-1 flex-col items-center justify-center gap-10'>
       <h1>Welcome to the home page!</h1>
-    </>
+      <Link to={'/products'}>
+        <Button className='capitalize' color='primary' size='lg'>
+          Check our products
+        </Button>
+      </Link>
+    </div>
   )
 }
 

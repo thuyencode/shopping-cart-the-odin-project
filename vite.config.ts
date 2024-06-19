8cfbab34e7ca21ae215b06b406b92e7984f9cdf5
Thuyen Code <thuyencode@proton.me>
2024-06-19 14:13:11 +0700

    Fonts are directly download from Bunny Fonts' servers from now

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
 import simpleHtmlPlugin from 'vite-plugin-simple-html'
-import { webfontDownload } from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/shopping-cart-the-odin-project',
-  plugins: [react(), simpleHtmlPlugin({ minify: true }), webfontDownload()],
+  plugins: [react(), simpleHtmlPlugin({ minify: true })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')

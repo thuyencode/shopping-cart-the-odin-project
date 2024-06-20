e56980fe54d89f04aac2714cf24fdbbde2f91486
Thuyen Code <thuyencode@proton.me>
2024-06-20 10:40:47 +0700

    Make the footer more responsive

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,8 +4,8 @@ import { Footer as DaisyFooter, Link } from 'react-daisyui'
 
 function Footer(): ReactElement {
   return (
-    <DaisyFooter className='flex justify-center bg-neutral p-4 text-neutral-content'>
-      <DaisyFooter.Title className='m-0'>
+    <DaisyFooter className='block justify-center bg-neutral p-2 text-center text-neutral-content md:flex md:p-4'>
+      <DaisyFooter.Title className='m-0 inline-block max-md:text-xs'>
         <Link
           href='https://github.com/thuyencode/shopping-cart-the-odin-project'
           target='_blank'
@@ -14,8 +14,11 @@ function Footer(): ReactElement {
           Thuyen Code
         </Link>
       </DaisyFooter.Title>
-      <Icon className='text-xl' icon={'mdi:copyright'} />
-      <DaisyFooter.Title className='m-0'>
+      <Icon
+        className='inline-block text-base max-md:mb-[1px] max-md:ml-1 md:text-xl'
+        icon={'mdi:copyright'}
+      />
+      <DaisyFooter.Title className='m-0 block max-md:mt-1 max-md:text-xs'>
         <Link
           href={import.meta.env.VITE_API_URL}
           target='_blank'

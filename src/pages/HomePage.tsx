ec979eceec610fb5cb722e61147b9f4482378f1a
Thuyen Code <thuyencode@proton.me>
2024-06-21 11:46:02 +0700

    Smaller button on small screen

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 
 function HomePage(): ReactElement {
   return (
-    <Hero.Content className='flex-col gap-7'>
+    <Hero.Content className='flex-col gap-7 text-center'>
       <h1>Welcome to the home page!</h1>
 
       <Link to={'/products'}>
-        <Button className='gap-1 capitalize' color='primary' size='lg'>
+        <Button className='gap-1 capitalize md:btn-lg' color='primary'>
           Check our products
-          <Icon className='text-xl' icon={'mdi:shopping'} />
+          <Icon className='text-lg md:text-xl' icon={'mdi:shopping'} />
         </Button>
       </Link>
     </Hero.Content>

bc595a8d4615705b6f5e929d11a0119f0fbaad24
Thuyen Code <thuyencode@proton.me>
2024-07-05 09:33:17 +0700

    Fix the loading ui

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { createPortal } from 'react-dom'
 
 function Loading(): ReactPortal {
   return createPortal(
-    <div className='sticky inset-0 z-10 flex h-screen items-center justify-center backdrop-blur-sm'>
+    <div className='fixed inset-0 z-10 flex items-center justify-center backdrop-blur-sm'>
       <span className='loading loading-spinner size-20' />
     </div>,
     document.body

3b9fb4349c01be28d3c4a46966f1b394e57023c0
Thuyen Code <thuyencode@proton.me>
2024-07-05 16:59:51 +0700

    Correct the error message of useFiltersContext hook

--- src/pages/Products/components/FiltersBar/FiltersBar.hook.ts
+++ src/pages/Products/components/FiltersBar/FiltersBar.hook.ts
@@ -5,7 +5,7 @@ export default function useFiltersContext(): FiltersContextState {
   const context = useContext(FiltersContext)
 
   if (context == null) {
-    throw new Error('Must be used within FiltersContextProvider')
+    throw new Error('Must be used within FiltersBar')
   }
 
   return context

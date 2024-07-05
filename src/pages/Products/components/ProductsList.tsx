88ba0d97dcf72290c79f4ae0d01f136bc1f9e342
Thuyen Code <thuyencode@proton.me>
2024-07-05 08:53:00 +0700

    Move ProductCard into its own file

--- src/pages/Products/components/ProductsList.tsx
+++ src/pages/Products/components/ProductsList.tsx
@@ -1,7 +1,7 @@
 import { type Product } from '@/lib/types'
-import { Icon } from '@iconify/react'
 import { type ReactElement } from 'react'
-import { Link, useLoaderData } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
+import ProductCard from './ProductCard'
 
 function ProductsList(): ReactElement {
   const { products } = useLoaderData() as { products: Product[] }
@@ -18,42 +18,6 @@ function ProductsList(): ReactElement {
         />
       ))}
     </div>
-  )
-}
-
-function ProductCard(
-  props: Pick<Product, 'image' | 'title' | 'price' | 'id'>
-): ReactElement {
-  return (
-    <Link to={props.id.toString()}>
-      <div
-        className='card card-bordered tooltip card-compact tooltip-info max-w-64 bg-base-300 shadow-lg'
-        role='tooltip'
-        aria-label='Card'
-        data-tip={props.title}
-      >
-        <figure>
-          <img
-            className='aspect-square object-cover object-top duration-300 hover:scale-105'
-            src={props.image}
-          />
-        </figure>
-
-        <div className='card-body gap-5'>
-          <div className='card-title justify-between text-base font-normal capitalize'>
-            <span className='truncate font-medium'>{props.title}</span>
-            <span>{props.price}$</span>
-          </div>
-
-          <div className='card-actions'>
-            <button className='btn btn-neutral btn-block'>
-              Check this product
-              <Icon className='text-xl' icon={'mdi:shopping'} />
-            </button>
-          </div>
-        </div>
-      </div>
-    </Link>
   )
 }
 
